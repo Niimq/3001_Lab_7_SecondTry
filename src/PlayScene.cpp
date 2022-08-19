f7b3e35bcb6e035f97bdd180eb7df5ef3c44838c
Niimq <Nima.Abbasi2@georgebrown.ca>
2022-08-19 11:49:15 -0400

    [ADD] Removed DecisionTree from PlayeScene

--- src/PlayScene.cpp
+++ src/PlayScene.cpp
@@ -55,9 +55,6 @@ void PlayScene::Update()
 		m_checkAllNodesWithBoth(); 
 		break;
 	}
-
-	// Make a Decision
-	m_decisionTree->MakeDecision();
 }
 
 void PlayScene::Clean()
@@ -359,9 +356,6 @@ void PlayScene::Start()
 	m_toggleGrid(m_isGridEnabled);
 
 	// Create Decision Tree
-	m_decisionTree = new DecisionTree(m_pStarship); // using our overloaded constructor
-	m_decisionTree->Display(); // optional
-	m_decisionTree->MakeDecision(); // Patrol
 
 	// Pre-load sounds
 	SoundManager::Instance().Load("../Assets/audio/yay.ogg", "yay", SoundType::SOUND_SFX);
